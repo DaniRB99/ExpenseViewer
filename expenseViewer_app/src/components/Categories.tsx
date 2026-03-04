82efa5d8c978940d661e7c414d1ccadcdb612916
Daniel Rocamora Bru <dani.rocamora.99@gmail.com>
2026-03-04 21:03:55 +0100

    Form para añadir categorías. Falta renderización dinámica

--- expenseViewer_app/src/components/Categories.tsx
+++ expenseViewer_app/src/components/Categories.tsx
@@ -1,6 +1,37 @@
+import { useState, type ChangeEvent, type ChangeEventHandler, type FormEvent } from "react"
 import "./Categories.css"
+import CategoryForm from "./CategoryForm";
+
+//TODO: onClick del button -> form de nueva categoría (incluido en el UL)
+//TODO: envío del form -> envío al back y crear nuevo LI
+//          Para ello los elementos LI tienen que ser una lista renderizada
+//          
+
 
 function Categories() {
+    const [toggleForm, setToggleForm] = useState<boolean>(true);
+
+    const showForm = () => {
+        setToggleForm(!toggleForm)
+    }
+
+    const [newCategory, setNewCategory] = useState<String>("");
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("Buena categoría ahí " + newCategory);
+
+        //TODO: llamada al back con el POST y generar nueva línea
+        //TODO: los <li> tienen que ser una lista renderizada
+
+        showForm()
+    }
+
+    const handleNewCategory = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        setNewCategory(e.target.value)
+    }
+
     return (
         <>
             <div className="sub-panel">
@@ -13,8 +44,9 @@ function Categories() {
                     <li>Caprichos</li>
                     <li>Almuerzos</li>
                     <li>Psicólogo</li>
+                    {toggleForm && CategoryForm({ handleNewCategory, handleSubmit })}
 
-                    <button className="add">➕</button>
+                    <button className="add" onClick={showForm}>➕</button>
                 </ul>
             </div>
         </>
---
+++ expenseViewer_app/src/components/CategoryForm.tsx
@@ -0,0 +1,27 @@
+import { useState, type ChangeEvent, type FormEvent } from 'react'
+
+interface props {
+    handleNewCategory: (e:ChangeEvent<HTMLInputElement>) => void,
+    handleSubmit: (e:FormEvent<HTMLFormElement>) => void
+}
+
+function CategoryForm({handleNewCategory, handleSubmit}:props) {
+
+
+    return (
+        <>
+            <li>
+                <form onSubmit={handleSubmit}>
+                    <fieldset>
+                        <input type="string" onChange={handleNewCategory}></input>
+                    </fieldset>
+                </form>
+            </li>
+        </>
+    )
+    // return (
+    //     <li>Buenas tardes</li>
+    // )
+}
+
+export default CategoryForm
